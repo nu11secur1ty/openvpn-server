4eed9566e877ac4d795d4aab63669c61d2894755
nu11secur1ty <venvaropt@gmail.com>
2020-10-08 17:22:41 +0300

    Update openvpn-install.sh

--- Ubuntu20.04/openvpn-install.sh
+++ Ubuntu20.04/openvpn-install.sh
@@ -3,6 +3,7 @@
 # https://github.com/Nyr/openvpn-install
 #
 # Copyright (c) 2013 Nyr. Released under the MIT License.
+# Modified @nu11secur1ty
 
 
 # Detect Debian users running the script with "sh" instead of bash
