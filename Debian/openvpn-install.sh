99b4e9964dc26037f5a6d143bf2698c428e268f0
nu11secur1ty <venvaropt@gmail.com>
2020-12-19 21:40:07 +0200

    Update openvpn-install.sh

--- Debian/openvpn-install.sh
+++ Debian/openvpn-install.sh
@@ -543,6 +543,8 @@ else
 				rm -f /etc/sysctl.d/30-openvpn-forward.conf
 				if [[ "$os" = "debian" || "$os" = "ubuntu" ]]; then
 					apt-get remove --purge -y openvpn
+					# Cleaning for Debian and Ubuntu
+					rm -rf /etc/openvpn/
 				else
 					# Else, OS must be CentOS or Fedora
 					yum remove -y openvpn
