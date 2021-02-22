93c6592b33db6340e9172ba94d6e9005eccb6eca
nu11secur1ty <venvaropt@gmail.com>
2021-02-22 16:13:53 +0200

    Update cron.sh

--- CentOS_Ubuntu20.04/stunnel/MacOS/cron.sh
+++ CentOS_Ubuntu20.04/stunnel/MacOS/cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # * * * * * bash /opt/cron.sh > /dev/null 2>&1
-# systemctl restart stunnel4.service
+systemctl restart stunnel4.service
 systemctl status stunnel4.service
 systemctl status openvpn@server.service
   exit 0;
