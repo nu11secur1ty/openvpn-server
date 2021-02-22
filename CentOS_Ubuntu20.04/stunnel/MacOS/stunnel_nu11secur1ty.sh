95aa654297cfbd7a00ee6885e844c75ce68b266e
nu11secur1ty <venvaropt@gmail.com>
2021-02-22 16:53:42 +0200

    Update stunnel_nu11secur1ty.sh

--- CentOS_Ubuntu20.04/stunnel/MacOS/stunnel_nu11secur1ty.sh
+++ CentOS_Ubuntu20.04/stunnel/MacOS/stunnel_nu11secur1ty.sh
@@ -4,3 +4,12 @@ cd /usr/local/Homebrew/Library/Tabs/homebrew
 rm -rf homebrew-core
 git clone https://github.com/Homebrew/homebrew-core.git
 brew install stunnel
+
+# EOF here
+#[openvpn-1]
+#cert = /usr/local/etc/stunnel/stunnel.pem
+#client = yes
+#accept = 1194
+#connect = x.x.x.x:443
+
+brew service restart stunnel
