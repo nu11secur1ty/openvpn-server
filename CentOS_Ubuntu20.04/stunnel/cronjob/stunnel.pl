b9d5e7139829dc44520478dcead697aead0e7114
nu11secur1ty <venvaropt@gmail.com>
2021-02-24 16:42:42 +0200

    Update stunnel.pl

--- CentOS_Ubuntu20.04/stunnel/cronjob/stunnel.pl
+++ CentOS_Ubuntu20.04/stunnel/cronjob/stunnel.pl
@@ -5,6 +5,7 @@ use warnings;
 use diagnostics;
 no warnings 'numeric';
 
+
 my $status_not_ok = 0;
 my $check_status = `pgrep -f stunnel`;
 	if ($check_status == 0){
@@ -13,11 +14,13 @@ my $check_status = `pgrep -f stunnel`;
 	}else{
 		print "Status already started\n";
 	}
+
 # Starting the service
 	if ($status_not_ok == 1) {
 		my $starting = `systemctl start stunnel4.service`;
 		print "The problem is fixed: $check_status\n";
 	}
+
 # Listening Service:
 my $bad_status = "refused";
 my $stunnel_already = `systemctl status stunnel4.service | grep -o "already"`;
@@ -34,7 +37,7 @@ my $g00d_status = "already";
 my $stunnel_refused = `systemctl status stunnel4.service | grep -o "refused"`;
 
 my $already_L_443_fix = $stunnel_refused eq $bad_status;
-if ($already_L_443_fix == 1){
+if ($already_L_443_fix == 0){
 	my $stunnel_reload = `systemctl restart stunnel4.service`;
 	print "The stunel listening on port 443 is runing\n";
 }else{
