c59b33081110862c3531d8567254d4befefb0ae5
nu11secur1ty <venvaropt@gmail.com>
2021-03-09 14:25:00 +0200

    Update sunnellatest.sh

--- CentOS_Ubuntu20.04/stunnel/sunnellatest.sh
+++ CentOS_Ubuntu20.04/stunnel/sunnellatest.sh
@@ -11,26 +11,21 @@ wget https://www.stunnel.org/downloads/stunnel-5.58.tar.gz
 tar -xvf stunnel-5.58.tar.gz 
 cd stunnel-5.58
 ./configure && make && make install
-
-cd /etc/stunnel/
-openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=BG' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
+cd /usr/local/etc/stunnel
+openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=BG' -keyout /usr/local/etc/stunnel/stunnel.pem -out /usr/local/etc/stunnel/stunnel.pem
 touch stunnel.conf
-
-place=whereis stunnel
-echo "$place"
-
 # echo "client = no" # if you don't have an account yet
-echo "client = yes" |  tee -a /etc/stunnel/stunnel.conf
-echo "[openvpn]" |  tee -a /etc/stunnel/stunnel.conf
-echo "accept = 443" |  tee -a /etc/stunnel/stunnel.conf
-echo "connect = 127.0.0.1:1194" |  tee -a /etc/stunnel/stunnel.conf
-echo "cert = /etc/stunnel/stunnel.pem" |  tee -a /etc/stunnel/stunnel.conf
+echo "client = yes" |  tee -a /usr/local/etc/stunnel/stunnel.conf
+echo "[openvpn]" |  tee -a /usr/local/etc/stunnel/stunnel.conf
+echo "accept = 443" |  tee -a /usr/local/etc/stunnel//stunnel.conf
+echo "connect = 127.0.0.1:1194" |  tee -a /usr/local/etc/stunnel/stunnel.conf
+echo "cert = /etc/stunnel/stunnel.pem" |  tee -a /usr/local/etc/stunnel/stunnel.conf
 
  sed -i -e 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
- cp /etc/stunnel/stunnel.pem ~
+ cp /usr/local/etc/stunnel/stunnel.pem ~
 
 # download stunnel.pem from home directory. It is needed by client.
- service stunnel4 restart
+ service stunnel restart
 
 
