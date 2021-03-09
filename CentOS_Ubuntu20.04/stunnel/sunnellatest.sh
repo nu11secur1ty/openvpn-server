585baa518c1520a04e373622e0f952218cd4525d
nu11secur1ty <venvaropt@gmail.com>
2021-03-09 14:08:45 +0200

    Update sunnellatest.sh

--- CentOS_Ubuntu20.04/stunnel/sunnellatest.sh
+++ CentOS_Ubuntu20.04/stunnel/sunnellatest.sh
@@ -7,6 +7,7 @@ apt install -y stunnel4
 cd /etc/stunnel/
 openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=BG' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
 touch stunnel.conf
+
 # echo "client = no" # if you don't have an account yet
 echo "client = yes" |  tee -a /etc/stunnel/stunnel.conf
 echo "[openvpn]" |  tee -a /etc/stunnel/stunnel.conf
@@ -17,10 +18,13 @@ echo "cert = /etc/stunnel/stunnel.pem" |  tee -a /etc/stunnel/stunnel.conf
  sed -i -e 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
  cp /etc/stunnel/stunnel.pem ~
+
 # download stunnel.pem from home directory. It is needed by client.
  service stunnel4 restart
+
 # New version
-cd /etc/stunnel/
+cd /opt/
+apt-get install libssl-dev -y
 wget https://www.stunnel.org/downloads/stunnel-5.58.tar.gz
 tar -xvf stunnel-5.58.tar.gz 
 cd stunnel-5.58
