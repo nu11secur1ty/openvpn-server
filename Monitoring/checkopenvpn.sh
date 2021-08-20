4e93b89cd35db18c926a0d95c1472be1c3669dcc
nu11secur1ty <venvaropt@gmail.com>
2021-08-20 14:41:58 +0300

    Create checkopenvpn.sh

---
+++ Monitoring/checkopenvpn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# @nu11secur1ty
+
+# timestamp
+ts=`date +%m-%d-%Y_%H:%M:%S`
+
+if pgrep "openvpn" > /dev/null
+then
+    echo "$ts: OpenVPN running..." >> /etc/openvpn/myscript/processcheck.log
+else
+    echo "$ts: openvpn not running, restarting..." >> /etc/openvpn/myscript/processcheck.log 
+    /etc/init.d/openvpn start > /dev/null >> /etc/openvpn/myscript/processcheck.log
+
+    if pgrep "openvpn" > /dev/null
+    then
+        echo "$ts: openvpn started OK..." >> /etc/openvpn/myscript/processcheck.log
+        echo "OpenVPN service went down, the service has been restarted." | mail -s "OpenVPN went down" email@domain.com
+    else
+        echo "$ts: openvpn not started..." >> /etc/openvpn/myscript/processcheck.log
+        echo "OpenVPN service is down, the server is trying to restart the service." | mail -s "OpenVPN is down" email@domain.com
+    fi
+fi
